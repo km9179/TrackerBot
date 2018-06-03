87e7241cf208e517a2335d3dc9c4b93c221caceb
rajabhi21 <abhi.raj21jan@gmail.com>
2018-06-03 17:09:40 +0530

    layout changed

--- app/src/main/java/app/khushbu/trackerbot/Activity2.java
+++ app/src/main/java/app/khushbu/trackerbot/Activity2.java
@@ -1,7 +1,9 @@
 package app.khushbu.trackerbot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Intent intent=getIntent();
+        String z=intent.getStringExtra("button");
+
+        TextView textView=(TextView)findViewById(R.id.textView);
+        textView.setText(z);
     }
 }
--- app/src/main/java/app/khushbu/trackerbot/MainActivity.java
+++ app/src/main/java/app/khushbu/trackerbot/MainActivity.java
@@ -1,27 +1,33 @@
 package app.khushbu.trackerbot;
 
 import android.content.Intent;
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import static app.khushbu.trackerbot.R.layout.activity1;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button button1;
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity1);
-        button1=(Button)findViewById(R.id.codeforces);
-        button1.setOnClickListener(this);
+        button=(Button)findViewById(R.id.codeforces);
     }
 
-    @Override
     public void onClick(View view) {
         Intent intent=new Intent(this,Activity2.class);
+        String z= "fhb";
+        z=view.getTag().toString();
+        intent.putExtra("button",z);
         startActivity(intent);
+
+        Log.i("button",z);
 
     }
 }
