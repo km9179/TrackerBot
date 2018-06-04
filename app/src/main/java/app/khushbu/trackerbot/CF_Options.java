9c11efe53a760aaefec3c9655198884063426739
Khushbu <khushbu999@github.gov>
2018-06-04 09:09:27 +0530

    3rd commit

---
+++ app/src/main/java/app/khushbu/trackerbot/CF_Options.java
@@ -0,0 +1,23 @@
+package app.khushbu.trackerbot;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class CF_Options extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.cf_options_frag,null);
+        return v;
+    }
+}
