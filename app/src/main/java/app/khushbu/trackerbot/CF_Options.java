f16e7372a475bbc747f7b3d1b557af80489792ba
Khushbu <khushbu999@github.gov>
2018-06-24 18:41:50 +0530

    13th by khushbu

--- app/src/main/java/app/khushbu/trackerbot/CF_Options.java
+++ app/src/main/java/app/khushbu/trackerbot/CF_Options.java
@@ -13,16 +13,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 
 public class CF_Options extends android.app.DialogFragment implements View.OnClickListener{
-    Button cfContest;
+    Button cfContest,cfCalender;
     public static int tag;
+    static Map url;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.cf_options_frag,null);
         cfContest=(Button)v.findViewById(R.id.contests);
+        cfCalender=(Button)v.findViewById(R.id.calender);
         cfContest.setOnClickListener(this);
+        cfCalender.setOnClickListener(this);
+        url=new TreeMap();
+        url.put(1,"http://codeforces.com/");
+        url.put(2,"http://codechef.com/");
+        url.put(12,"http://topcoder.com/");
+        url.put(73,"http://hackerearth.com/");
+        url.put(63,"http://hackerrank.com/");
         return v;
     }
 
@@ -33,9 +45,20 @@ public class CF_Options extends android.app.DialogFragment implements View.OnCli
 
     @Override
     public void onClick(View view) {
-        Intent intent=new Intent(getActivity(),ContestListActivity.class);
-        intent.putExtra("id",tag);
-        startActivity(intent);
-        dismiss();
+        int id=view.getId();
+        if(id==R.id.contests) {
+            Intent intent = new Intent(getActivity(), ContestListActivity.class);
+            intent.putExtra("id", tag);
+            startActivity(intent);
+            dismiss();
+        }
+        else if(id==R.id.calender)
+        {
+            Intent intent = new Intent(getActivity(), ContestListActivity.class);
+           // intent.putExtra("url",url.);
+            startActivity(intent);
+            dismiss();
+
+        }
     }
 }
