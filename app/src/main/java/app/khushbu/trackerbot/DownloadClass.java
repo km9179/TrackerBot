eb16ed7eff72f0c8b519149ebdc44bcb341499d4
rajabhi21 <abhi.raj21jan@gmail.com>
2018-07-05 17:35:59 +0530

    date 24/06/2018 all ongoing contest layout changed

--- app/src/main/java/app/khushbu/trackerbot/DownloadClass.java
+++ app/src/main/java/app/khushbu/trackerbot/DownloadClass.java
@@ -12,8 +12,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
 import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class DownloadClass {
 
@@ -55,6 +64,7 @@ public class DownloadClass {
     }
     public void downloadTask(RequestQueue requestQueue, final int frag, final int siteKey){
 
+        disableSSLCertificateChecking();
         //to clear the screen
         clear(frag);
         this.siteKey=siteKey;
@@ -146,7 +156,7 @@ public class DownloadClass {
     public void downloadContestTask(RequestQueue requestQueue){
 
         //to clear the screen
-
+        disableSSLCertificateChecking();
         JsonObjectRequest objectRequest=new JsonObjectRequest(Request.Method.GET, this.url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
@@ -223,5 +233,34 @@ public class DownloadClass {
 
     public void setFlag(int flag) {
         this.flag = flag;
+    }
+    private static void disableSSLCertificateChecking() {
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
+        } };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
