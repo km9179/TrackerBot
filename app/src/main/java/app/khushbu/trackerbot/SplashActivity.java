0417ac18819400fbf0f309ded638c357f9e91a0f
rajabhi21 <abhi.raj21jan@gmail.com>
2018-07-08 03:08:10 +0530

    progress bar error message display

--- app/src/main/java/app/khushbu/trackerbot/SplashActivity.java
+++ app/src/main/java/app/khushbu/trackerbot/SplashActivity.java
@@ -7,15 +7,25 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN_TIME_OUT=4000;
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+        RequestQueue requestQueue = Volley.newRequestQueue(SplashActivity.this);
+        DownloadClass downloadClass = new DownloadClass();
+        downloadClass.setUrl("https://clist.by/api/v1/json/contest/?username=abhi&api_key=8f62bf40d07bb9af09535a22f21653ace0da43a4&order_by=-end&limit=100&offset=100");
+        downloadClass.downloadContestTask(requestQueue);
+        downloadClass.setFlag(3);
+        downloadClass.setUrl("https://clist.by/api/v1/json/contest/?username=abhi&api_key=8f62bf40d07bb9af09535a22f21653ace0da43a4&order_by=-end&limit=100&offset=0");
+        downloadClass.downloadContestTask(requestQueue);
     }
     @Override
     protected void onStart() {
